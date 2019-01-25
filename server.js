d59a692b07954d984127c95544b59c1e6ba87247
Tomasz Wylężek <tomwylezek@gmail.com>
2019-01-25 19:34:30 +0100

    Setup handlebars

--- server.js
+++ server.js
@@ -1,19 +1,23 @@
 const express = require("express");
-
+const hbs = require("hbs");
 const app = express();
 
+app.set("view engine", "hbs");
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
-  // res.send("<h1>Hello Express</h1>");
-  res.send({
-    name: "tom",
-    likes: ["biking", "cites"]
-  });
+  res.render('home.hbs', {
+    pageTitle: "Home page",
+    currentYear: new Date().getFullYear(),
+    welcomeMessage: 'Hello guys'
+  })
 });
 
 app.get("/about", (req, res) => {
-  res.send("About page");
+  res.render("about.hbs", {
+    pageTitle: "About page",
+    currentYear: new Date().getFullYear()
+  });
 });
 
 app.get("/bad", (req, res) => {
@@ -23,5 +27,5 @@ app.get("/bad", (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Server is up on port 3000')
+  console.log("Server is up on port 3000");
 });
