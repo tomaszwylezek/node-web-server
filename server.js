2d671eecd82e8acc42a477622ede780c7dc55503
Tomasz Wylężek <tomwylezek@gmail.com>
2019-01-25 19:14:07 +0100

    Setup express

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  // res.send("<h1>Hello Express</h1>");
+  res.send({
+    name: "tom",
+    likes: ["biking", "cites"]
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.send("About page");
+});
+
+app.get("/bad", (req, res) => {
+  res.send({
+    errorMessage: "Unable to handle request"
+  });
+});
+
+app.listen(3000);
